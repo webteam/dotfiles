d55390f236b6d435d2d990bbc9ef8fb2749f22ae
Christian Mayer <christian.mayer@webteam.at>
2012-05-06 17:41:40 +0200

    added rakefile to automate installation

--- bin/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )";
-
-# Install vim config
-echo " * Symlinking vim config..."
-ln -s $SCRIPT_DIR/../vimrc ~/.vimrc
-ln -s $SCRIPT_DIR/../vim ~/.vim
